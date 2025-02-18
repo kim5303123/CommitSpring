75706d8676dd999d621cdba62a760789653d868f
kim5303123 <kim5303123@gmail.com>
2025-02-18 17:24:46 +0900

    0218[승룡] 충돌난거 3개 commit

--- src/main/java/com/commit/restserver/CommitSpringApplication.java
+++ src/main/java/com/commit/restserver/CommitSpringApplication.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @MapperScan(basePackages = "com.commit.mappers")
-@ComponentScan(basePackages = {"com.commit"} )
+@ComponentScan(basePackages = { "com.commit" })
 public class CommitSpringApplication {
 
 	public static void main(String[] args) {
