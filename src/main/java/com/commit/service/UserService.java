190144dc15966b8751458f0ebfdaa7893dc187dd
MOONMARY <firstmoon5@naver.com>
2025-02-21 17:55:32 +0900

    0221 [호정] 회원정보 수정 기능

--- src/main/java/com/commit/service/UserService.java
+++ src/main/java/com/commit/service/UserService.java
@@ -23,14 +23,14 @@ public class UserService {
 	
 	@Autowired
 	private UserMapper userMapper;
-	@Autowired
-	private DegreeMapper degreeMapper;
-	@Autowired
-	private FamilyMapper familyMapper;
-	@Autowired
-	private ProfileMapper profileMapper;
-	@Autowired
-	private CareerMapper careerMapper;
+//	@Autowired
+//	private DegreeMapper degreeMapper;
+//	@Autowired
+//	private FamilyMapper familyMapper;
+//	@Autowired
+//	private ProfileMapper profileMapper;
+//	@Autowired
+//	private CareerMapper careerMapper;
 	
 	//	회원가입
 	public UserVo insertUser(UserVo user) {
@@ -42,10 +42,31 @@ public class UserService {
 		return user;
 	}
 	
+	
 	//	로그인
 	public UserVo login(UserVo user) {
         return userMapper.login(user);
 	}
+	
+	
+	// 회원 정보 변경
+	public UserVo updateUser(UserVo userVo) {
+		userMapper.updateUser(userVo);
+		return userVo;
+	}
+	
+	
+	// 전체 회원 목록
+	public UserVo selectAllUser(UserVo userVo) {
+		return userMapper.selectAllUser(userVo);
+	}
+	
+	
+	
+	
+	
+	
+	
 	
 //		학위정보 insert
 //    public DegreeVo insertDegree(DegreeVo degreeVo ) {
@@ -57,7 +78,7 @@ public class UserService {
 //        return degree;
 //	}
     
-    //	가족정보 insert
+//    //	가족정보 insert
 //    public FamilyVo insertFamily(FamilyVo family) { 
 //        familyMapper.familyInsert2(family); 
 //        return family;
